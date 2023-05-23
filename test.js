e25f51d63bd4fc504d1442c5b897651f3446769a
Jeremy <jeremy.ohearn1@gmail.com>
2023-05-23 17:23:34 -0400

    added new files and code

---
+++ test.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let https = require('https');
+let myPhotoLocation = "https://raw.githubusercontent.com/LearnWebCode/welcome-to-git/master/images/dog.jpg";
+
+fs.writeFile(__dirname + "/index.html", "<h1>HTML is great</h1>", function(error) {
+    if(error) {
+        return console.log(error);
+    } else {
+        return console.log("Congrats")
+    }
+});
+
+https.get(myPhotoLocation, function(response) {
+    response.pipe(fs.createWriteStream(__dirname + "/mydog.jpg"));
+});
+
