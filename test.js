961907249db25216f1c6065b146168239bc17d64
Jeremy <jeremy.ohearn1@gmail.com>
2023-05-23 18:00:52 -0400

    added code to style sheet and js file

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let https = require('https');
 let myPhotoLocation = "https://raw.githubusercontent.com/LearnWebCode/welcome-to-git/master/images/dog.jpg";
+let name = 'jeremy';
 
 fs.writeFile(__dirname + "/index.html", "<h1>HTML is great</h1>", function(error) {
     if(error) {
